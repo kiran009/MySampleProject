14f35f3d1522339748618833110bc8e04dec3227
Kiran Kumar Daadhi <kiran.daadhi@evolving.com>
2015-09-22 15:38:48 +0530

    Idea files

---
+++ example.py
@@ -0,0 +1,9 @@
+# Test program
+a = 25
+for i in range(a):
+    print("Value of i is",i)
+x = int(input("Enterer an integer value"))
+if(x<=10):
+    print("Number is less than or equal to 10")
+else:
+    print("Doesn't match")
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 __author__ = 'kkdaadhi'
 print("Hello")
-
 print ("World")
